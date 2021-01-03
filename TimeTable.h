6ae654358816229b381d2e1645f364a08be400a4
Alessandro <alecanova00@icloud.com>
2021-01-03 10:47:55 +0100

    Aggiunto metodo per la conversione dell'orario

--- TimeTable.h
+++ TimeTable.h
@@ -7,12 +7,30 @@
 
 #include "Train.h"
 #include <vector>
+
+static std::vector<int> timeConversion(int time){
+    std::vector<int> v(3);
+    int hour = (int)time/60;
+
+    if(hour >23){
+        v[3]=1;
+        hour-=24;
+    }else v[3]=0;
+    v[0] = hour;
+
+    int minutes = (int)time%60;
+    v[1]=minutes;
+    return v;
+
+}
+
 class TrainTimeTable{
 private:
     Train train;
     int stationType;
     std::vector<int> times;
 public:
+
     TrainTimeTable(Train t, int st, std::vector<int> tm);
 
     void setOffset(); //imposta il ritardo
