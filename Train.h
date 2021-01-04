21bde2d2a5892b176a3a62f0297a59c35a0bc8d1
ALESSANDRO VISENTIN <alessandro.visentin.10@studenti.unipd.it>
2021-01-04 09:53:13 +0100

    Update Train.h

--- Train.h
+++ Train.h
@@ -1,14 +1,30 @@
-//
-// Created by Alessandro Canova on 01/01/21.
-//
 
-#ifndef ASSEGNAMENTO_2_TRAIN_H
-#define ASSEGNAMENTO_2_TRAIN_H
-
-
-class Train {
-
+class Train
+{
+	void move();
+	bool can_move();
+	Train_type get_type();
+	int get_max_speed();
+	int get_cruise_speed();
+	int get_actual_speed();
+	int get_train_number();
+	int get_delay();
+	Station get_actual_station();
+	Station get_next_station();
+	int get_remaining_time();
+private:
+	const int MAX_SPEED;
+	const int CRUISE_SPEED;
+	int actual_speed;
+	int rail_position;
+	int delay;
+	Timetable table;
+	Station actual_station;
+	Station next_statiom;
+	int next_station_distance;
 };
 
-
-#endif //ASSEGNAMENTO_2_TRAIN_H
+public enum Train_type
+{
+	{Regional = 1, High_speed = 2, Super_high_speed = 3};
+};
